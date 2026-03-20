e0a539575662f37112265d8bb6b448a11c8b58d5
Albina Gurung <albinakushu9545@gmail.com>
2026-03-20 21:28:41 +0545

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template
+from dbm import dumb
+import os
+from flask import Flask, render_template, request
+from main import pdf_to_img
+
 app=Flask(__name__)
+
+UPLOAD_FOLDER='static/uploads'
+OUTPUT_FOLDER='static/outputs'
+
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER']=OUTPUT_FOLDER
+
 @app.route('/')
 def home():
     return render_template('index.html')    
+
+@app.route('/convert',methods=["POST"])
+def convert():
+    file=request.files.get("pdf")
+    image_type=request.form.get("format")
+    
+    if file and file.filename!="":
+        os.makedirs(app.config['UPLOAD_FOLDER'],exist_ok=True)
+        file_path=os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
+        file.save(file_path)
+        output_images=pdf_to_img(file_path,image_type,app.config['OUTPUT_FOLDER'])
+        return render_template('index.html',images=output_images)
+    else:
+        return render_template('index.html',error="No file uploaded")
+
 if __name__=='__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 import fitz
 import os
 
-def pdf_to_img(pdf_path,image_type):
-    output_folder="output_images"
+def pdf_to_img(pdf_path,image_type,output_folder):
     os.makedirs(output_folder,exist_ok=True)
     doc=fitz.open(pdf_path)
     output_images=[]
@@ -22,10 +21,3 @@ def pdf_to_img(pdf_path,image_type):
     doc.close()
     return output_images
 
-if __name__=='__main__':
-    pdf_path="/Users/albinagurung/Desktop/MyPdf.pdf"
-    image_type="jpg"
-    files=pdf_to_img(pdf_path,image_type)
-    print('converted files:')
-    for f in files:
-        print(f)
